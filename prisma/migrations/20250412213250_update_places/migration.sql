bf2a97a546d0e90ccb5b948b7db2089892e89b86
alexusernamebruh <85073684+alexusernamebruh@users.noreply.github.com>
2025-04-16 20:31:58 -0400

    Delete prisma directory

--- prisma/migrations/20250412213250_update_places/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Place" ADD COLUMN     "ratingsSum" DOUBLE PRECISION NOT NULL DEFAULT 0;
--- prisma/migrations/20250415015146_add_ai_chat/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "aiChatHistory" (
-    "id" SERIAL NOT NULL,
-    "content" TEXT NOT NULL,
-    "userId" INTEGER NOT NULL,
-    "title" TEXT NOT NULL,
-
-    CONSTRAINT "aiChatHistory_pkey" PRIMARY KEY ("id")
-);
